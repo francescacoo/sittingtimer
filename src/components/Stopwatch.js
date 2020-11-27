5f421a68708437da03191ccd9ad100bc548e7cb5
Francesca Placido <francescacoo@gmail.com>
2020-11-27 13:38:15 +0100

    updated

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
-import "../App.css";
 
+import "../App.css";
+window.onbeforeunload = () => {
+  localStorage.removeItem('mydata0');
+  localStorage.removeItem('mydata1');
+}
 class Stopwatch extends Component {
   newcounter=0;
   state = {
@@ -8,7 +12,14 @@ class Stopwatch extends Component {
     timerStart: 0,
     timerTime: 0,
     counter:0,
+    selectedOption:''
 
+  };
+
+  handleOptionChange = changeEvent => {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
   };
 
   startTimer = () => {
@@ -27,20 +38,28 @@ class Stopwatch extends Component {
   stopTimer = () => {
     this.setState({ timerOn: false });
     clearInterval(this.timer);
-    localStorage.setItem('myData', this.state.timerTime );
-    localStorage.getItem('counter');
-
   };
-  resetTimer = () => {
 
+
+  resetTimer = () => {
+      var mycounter = this.state.counter +1;
     this.setState({
       timerStart: 0,
       timerTime: 0,
-      counter:this.state.counter + 1
+      counter:mycounter
     });
-    var data={"counter:":this.state.counter,"time":this.state.timerTime};
+    const { timerTime } = this.state;
+    let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+    let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+    let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+
+    let newtime=hours+":"+minutes+":"+seconds+":"+centiseconds;
+    var data= this.state.selectedOption+" time: "+newtime;
     var datavar="mydata"+this.state.counter;
-    localStorage.setItem(datavar, JSON.stringify(data));
+    localStorage.setItem(datavar, data);
+
+
 
   };
 
@@ -50,10 +69,21 @@ class Stopwatch extends Component {
     let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
     let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
     let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
-    return (
-      <div className="Stopwatch">
-        <div className="data">{localStorage.getItem('myData')}</div>
 
+    var history1;
+    var history2;
+
+    const historysitting =[]
+    for (var i=0;  i<=this.state.counter;i++){
+      
+      history1=localStorage.getItem('mydata'+i);
+
+      historysitting.push(<li key={i}>{history1}</li>)
+    }
+    return (
+      <div className="Stopwatch">        
+
+        <div className="history">{historysitting}</div>
         <div className="Stopwatch-header">Stopwatch</div>
         <div className="Stopwatch-display">
           {hours} : {minutes} : {seconds} : {centiseconds}
@@ -70,7 +100,67 @@ class Stopwatch extends Component {
         {this.state.timerOn === false && this.state.timerTime > 0 && (
           <button onClick={this.resetTimer}>Reset</button>
         )}
+<form>
+
+<div className="form-check">
+  <label>
+    <input
+      type="radio"
+      name="react-tips"
+      value="kneeling chair 1"
+      checked={this.state.selectedOption === "kneeling chair 1"}
+      onChange={this.handleOptionChange}
+      className="form-check-input"
+    />
+Kneeling chair 1
+  </label>
+</div>
+
+<div className="form-check">
+  <label>
+    <input
+      type="radio"
+      name="react-tips"
+      value="kneelingChair2"
+      checked={this.state.selectedOption === "kneelingChair2"}
+      onChange={this.handleOptionChange}
+      className="form-check-input"
+    />
+    Kneeling chair 2
+  </label>
+</div>
+
+<div className="form-check">
+  <label>
+    <input
+      type="radio"
+      name="react-tips"
+      value="standing"
+      checked={this.state.selectedOption === "standing"}
+      onChange={this.handleOptionChange}
+      className="form-check-input"
+    />
+    standing
+  </label>
+</div>
+
+<div className="form-check">
+  <label>
+    <input
+      type="radio"
+      name="react-tips"
+      value="moving"
+      checked={this.state.selectedOption === "moving"}
+      onChange={this.handleOptionChange}
+      className="form-check-input"
+    />
+    moving
+  </label>
+</div>
+
+</form>
       </div>
+      
     );
   }
 }
